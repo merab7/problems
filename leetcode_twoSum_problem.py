6914c44904d969c5af20762432c1359d4bebb070
merab7 <merabtodua7@gmail.com>
2025-01-06 21:27:01 +0400

    3sum start

--- leetcode_twoSum_problem.py
+++ leetcode_twoSum_problem.py
@@ -59,4 +59,34 @@ class Solution(object):
     here is the editorial form leetcode:
       https://leetcode.com/problems/two-sum/editorial
 
+
+here i will just add the 3sum problem brut force solution and will continue it tomorrow:
+
+class Solution(object):
+    def threeSum(self, nums):
+        result = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            j = i+1
+            k = len(nums)-1
+            while j<k:
+                total = nums[i] + nums[j] + nums[k]
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j+=1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j+=1
+
+                    while nums[j] == nums[j-1] and j < k:
+                        j+=1
+
+        return result
+
+
 """
